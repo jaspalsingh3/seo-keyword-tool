a5715cad08baf674bc0d36b424ecd00cadfc0279
jaspalsingh3 <jaspal@savedelete.com>
2025-07-21 22:55:43 +0530

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/seo-keyword-tool/', // IMPORTANT: This is crucial for GitHub Pages project pages
+})
